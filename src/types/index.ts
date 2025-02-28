3824b27a8fd8b2fc9f87c44d10e8b0c1927125d1
RuslanWeber <ruslan26812@gmail.com>
2025-02-28 19:50:01 +0300

    Update index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,50 @@
+
+export interface IProduct {
+    id: number;
+    description: string;
+    image: string;
+    title: string;
+    category: string;
+    price: number | null;
+}
+
+export interface IProductsData {
+    products: IProduct[];
+    preview: string | null;
+    order: IOrder;
+    basket: IProduct[];
+    clearBasket(): void;
+    addToBasket(item: IProduct): void;
+	deleteFromBasket(itemId: number): void;
+	isLotInBasket(item: IProduct): boolean;
+	getTotalPrice(): number;
+	getBasketIds(): number[];
+	getBasketLength(): number;
+	clearOrder(): void;
+	checkValidation(data: Record<keyof IOrder, string>): boolean;
+}
+
+export interface IOrder {
+	payment: TPayment;
+	email: string;
+	phone: string;
+	address: string;
+	items: string[];
+	total: number;
+}
+
+export interface IOrderResult {
+	id: string;
+	total: number;
+}
+
+export interface IBasket {
+    item: IBasketItem[];
+    price: number;
+}
+
+export type TCategory = 'софт-скил' | 'хард-скил' | 'кнопка' | 'дополнительное' | 'другое';
+export type TPayment =  'card' | 'cash';
+export type IBasketItem = Pick<IProduct, 'id' | 'title' | 'price'>;
+export type IShippingForm = Pick<IOrder, 'payment' | 'address'>
+export type IContactForm = Pick<IOrder, 'email' | 'phone'>
